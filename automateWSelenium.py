cbb80ae7a6724438e859faf9bcfd2ede07ad6cec
Matheus Ramos <devmatheusr@gmail.com>
2022-09-13 09:37:13 -0300

    merging

--- automateWSelenium.py
+++ automateWSelenium.py
@@ -3,6 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 
 driver = webdriver.Firefox()
@@ -10,4 +14,4 @@ driver.get("https://br.betano.com/casino/games/aviator/3337/?sbLoadEvent=true")
 time.sleep(10000)
 username = driver.find_element(By.NAME, 'username')
 username.click()
-username.send_keys('eotheuzin02@gmail.com')
+username.send_keys(username)
